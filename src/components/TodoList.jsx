1aea704734cedfe293f0fc9bf1ada95a50dde0a7
ngongoccamly04 <ngongoccamly414@gmail.com>
2025-12-15 23:54:35 +0700

    feat: complete Kanban board with drag & drop, calendar view, statistics

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,7 +6,6 @@ import { collection, query, where, onSnapshot, updateDoc, doc } from 'firebase/f
 import TodoForm from './Todo/TodoForm';
 import TodoCard from './Todo/TodoCard'; 
 
-import TodoForm from './Todo/TodoForm'; 
 import KanbanBoard from './Todo/KanbanBoard'; 
 import CalendarView from './Todo/CalendarView'; 
 import GalleryView from './Todo/GalleryView'; 
